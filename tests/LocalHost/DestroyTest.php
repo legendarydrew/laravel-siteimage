21289cb1a73ceb0db4222e8b0de5438e65c98572
Drew Maughan <drew.maughan@toptal.com>
2022-01-25 18:54:15 +0000

    feat: added tests for LocalHost destroy().

--- tests/LocalHost/DestroyTest.php
+++ tests/LocalHost/DestroyTest.php
@@ -3,9 +3,35 @@
 namespace PZL\SiteImage\Tests\LocalHost;
 
 
+use Illuminate\Filesystem\Filesystem;
+use PZL\SiteImage\Host\LocalHost;
 use PZL\SiteImage\Tests\TestCase;
 
 class DestroyTest extends TestCase
 {
+    /**
+     * @var LocalHost
+     */
+    private $provider;
 
+    private $image;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->provider = new LocalHost();
+        $this->image = $this->faker->image;
+
+        $fs = new Filesystem();
+        $fs->cleanDirectory($this->provider->getFolder());
+    }
+
+    public function testDestroy()
+    {
+        $public_id = basename($this->image);
+        $this->provider->destroy($public_id);
+
+        self::assertFileDoesNotExist($this->provider->getFolder() . $public_id);
+    }
 }
