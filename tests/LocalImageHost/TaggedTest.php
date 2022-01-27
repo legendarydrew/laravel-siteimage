283baebbc80cad501228f4056009b52bc21a37f3
Drew Maughan <drew.maughan@toptal.com>
2022-01-27 23:01:30 +0000

    test: tagged() for LocalImageHost.

--- tests/LocalImageHost/TaggedTest.php
+++ tests/LocalImageHost/TaggedTest.php
@@ -3,14 +3,56 @@
 namespace PZL\SiteImage\Tests\LocalImageHost;
 
 
+use Illuminate\Foundation\Testing\WithFaker;
+use PZL\SiteImage\Host\LocalImageHost;
 use PZL\SiteImage\Tests\TestCase;
 
 class TaggedTest extends TestCase
 {
+    use WithFaker;
+
+    /**
+     * @var LocalImageHost
+     */
+    private $provider;
+
+    /**
+     * @var string
+     */
+    private $image;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->provider = new LocalImageHost();
+    }
 
     public function testToBeImplemented()
     {
-        self::markTestIncomplete();
+        $untagged = array_map(function () {
+            return $this->provider->upload($this->faker->image);
+        }, range(1, $this->faker->numberBetween(1, 5)));
+        $tagged_one = array_map(function () {
+            return $this->provider->upload($this->faker->image, null, null, ['one']);
+        }, range(1, $this->faker->numberBetween(1, 5)));
+        $tagged_two = array_map(function () {
+            return $this->provider->upload($this->faker->image, null, null, ['two']);
+        }, range(1, $this->faker->numberBetween(1, 5)));
+
+        $results = $this->provider->tagged('one');
+
+        foreach ($untagged as $public_id) {
+            self::assertNotContains($public_id, $results);
+        }
+
+        foreach ($tagged_one as $public_id) {
+            self::assertContains($public_id, $results);
+        }
+
+        foreach ($tagged_two as $public_id) {
+            self::assertNotContains($public_id, $results);
+        }
     }
 
 }
