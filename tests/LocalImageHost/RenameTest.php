b9ba180784d1580278bc98701cccdd95ee6cda8b
Drew Maughan <drew.maughan@toptal.com>
2024-12-13 16:06:22 +0000

    test: adding a test suite for the rename method.

--- tests/LocalImageHost/RenameTest.php
+++ tests/LocalImageHost/RenameTest.php
@@ -8,6 +8,7 @@ namespace PZL\SiteImage\Tests\LocalImageHost;
 use PZL\Http\ResponseCode;
 use PZL\SiteImage\Host\LocalImageHost;
 use PZL\SiteImage\Tests\TestCase;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 /**
  * RejectTest.
@@ -28,9 +29,11 @@ class RenameTest extends TestCase
         $this->source_file = basename($image);
         $this->new_name    = $this->faker->word();
         copy($image, $this->provider->getFolder() . $this->source_file);
-
     }
 
+    /**
+     * @covers \PZL\SiteImage\Host\LocalImageHost
+     */
     public function testRename()
     {
         $response = $this->provider->rename($this->source_file, $this->new_name);
@@ -40,34 +43,41 @@ class RenameTest extends TestCase
     }
 
     /**
+     * @covers  \PZL\SiteImage\Host\LocalImageHost
      * @depends testRename
      */
     public function testRenameOverwriteFalse()
     {
-        $image             = $this->faker->image($this->provider->getFolder());
+        $image = $this->faker->image($this->provider->getFolder());
         copy($image, $this->provider->getFolder() . $this->new_name);
 
-        $this->expectExceptionCode(ResponseCode::RESPONSE_PRECONDITION_FAILED);
-        $this->provider->rename($this->source_file, $this->new_name, false);
+        $this->expectException(HttpException::class);
+        $this->provider->rename($this->source_file, $this->new_name, FALSE);
     }
 
     /**
+     * @covers  \PZL\SiteImage\Host\LocalImageHost
      * @depends testRename
      */
     public function testRenameOverwriteTrue()
     {
-        $image             = $this->faker->image($this->provider->getFolder());
+        $image = $this->faker->image($this->provider->getFolder());
         copy($image, $this->provider->getFolder() . $this->new_name);
 
-        $this->provider->rename($this->source_file, $this->new_name, TRUE);
+        $response = $this->provider->rename($this->source_file, $this->new_name, TRUE);
+
+        self::assertEquals($this->new_name, $response['public_id']);
+        self::assertFileDoesNotExist($this->provider->getFolder() . $this->source_file);
+        self::assertFileExists($this->provider->getFolder() . $this->new_name);
     }
 
     /**
+     * @covers  \PZL\SiteImage\Host\LocalImageHost
      * @depends testRename
      */
     public function testRenameInvalid()
     {
-        $this->expectException((string)ResponseCode::RESPONSE_NOT_FOUND);
+        $this->expectException(HttpException::class);
         $this->provider->rename(ResponseCode::RESPONSE_NOT_FOUND, $this->new_name);
     }
 }
